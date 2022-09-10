21ad53086406deb71612dc8c718ac775c731db39
Barbara <barbeaboo@gmail.com>
2022-09-10 16:04:42 +0000

    Update and correction

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+def islower(c):
+    if ord(c) > 97 and ord(c) < 126:
+        return(true)
+    else:
+        return(false)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                pwdpd
+                
