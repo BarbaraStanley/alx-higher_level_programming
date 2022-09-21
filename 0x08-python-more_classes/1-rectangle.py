d399e0ac2630366f5bc53dd852847e24bfb2e1f1
Barbara <barbeaboo@gmail.com>
2022-09-21 19:45:15 +0000

    Python more class, task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -37,7 +37,7 @@ class Rectangle:
         '''property setter'''
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -51,6 +51,6 @@ class Rectangle:
         '''property setter'''
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__height = value
