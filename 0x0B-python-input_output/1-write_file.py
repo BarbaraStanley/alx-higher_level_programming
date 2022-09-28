4fdb8379ecdb479367fd71e49edb59541eacf1b1
Barbara <barbeaboo@gmail.com>
2022-09-28 19:50:51 +0000

    Task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,7 @@
 
 
 def write_file(filename="", text=""):
+    ''' appends text to filename, and creates filename if it doesn't exist'''
     with open(filename, mode="x", encoding="utf-8") as new:
         new.write(text)
         print("{} characters".format(len(filename)))
