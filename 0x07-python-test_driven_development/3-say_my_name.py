a00924c9eb04a36b6337a72f1a3ae27907c2712f
Barbara <barbeaboo@gmail.com>
2022-10-08 14:21:35 +0000

    Task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,7 @@
 
 
 def say_my_name(first_name, last_name=""):
+    ''' Function to print full name'''
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
