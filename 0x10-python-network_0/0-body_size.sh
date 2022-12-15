557b38e8fbd2843a3a46119b7e416284bf2cda41
BarbaraStanley <barbeaboo@gmail.com>
2022-12-15 13:29:41 -0800

    correction task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-# Takes in a URL, sends a request to a URL and displayes the
-# size of the body of the respose
-
+#!/usr/bin/bash
+# Takes in a URL, sends a request to a URL and displayes the size of the request body
 curl -sI "$@" | grep 'Content-Length' | cut -d ' ' -f2
