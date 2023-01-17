936fc10438fcf6f8241d11cd3166c5635c7b70ab
BarbaraStanley <barbeaboo@gmail.com>
2023-01-17 23:45:30 +0000

    2nd push

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // script that writes a string to a file
 
-const args = process.argv
-const fs = require('fs')
-fs.writeFileSync(args[2], args[3], 'utf-8')
+const args = process.argv;
+const fs = require('fs');
+fs.writeFileSync(args[2], args[3], 'utf-8');
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 // script that display the status code of a GET request
 
-const args = process.argv
-const request = require('request')
-const url = args[2]
+const args = process.argv;
+const request = require('request');
+const url = args[2];
 
 request(url, function (error, response, body) {
- if (error) {
-  console.log(error);
- } else {
-  console.log('code: {}' + JSON.parse(body));
- }
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code: {}' + JSON.parse(body));
+  }
 });
