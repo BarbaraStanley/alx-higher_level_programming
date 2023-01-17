2f8d2f8794264de60a1a14ef211b4c1ef5bd4799
BarbaraStanley <barbeaboo@gmail.com>
2023-01-17 23:42:47 +0000

    first push

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// script that reads and prints the content of a file
+
+const args = process.argv
+const filesys = require ('fs')
+const str = filesys.readFileSync(args[2], 'utf-8')
+console.log(str)
