429b060fb5746c332d01f5067ad79cd631a08561
Barbara <barbeaboo@gmail.com>
2023-01-21 16:03:10 +0000

    3-4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' replaces an element in a list at a given index without modifying list'''
+
+def new_in_list(my_list, idx, element):
+    if idx >= 0 and idx < len(my_list):
+        new = []
+        for i in my_list:
+            new.append(i)
+        new[idx] = element
+        return new
