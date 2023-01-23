9366777eb4fe22a776760f0cb50da072e08780dd
Barbara <barbeaboo@gmail.com>
2023-01-23 00:58:18 +0000

    3 correction, semistandard bitc

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
 // prints title of a Star Wars movie where episode number matches given integer
 
-const args = process.argv
-const id = args[2]
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`
+const args = process.argv;
+const id = args[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-const request = require ('request')
-request(url, function(error, response, body){
-	console.log(JSON.parse(body).title)
-})
+const request = require('request');
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  console.log(JSON.parse(body).title);
+});
