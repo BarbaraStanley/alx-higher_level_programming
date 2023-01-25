a2cf101e270f980883b688dd32ac29f00380d9c7
Barbara <barbeaboo@gmail.com>
2023-01-25 21:53:52 +0000

    jQuery 0-3

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,6 @@
+// script that updates the text color of the <header> element to red (#FF0000)
+// when the user clicks on the tag DIV#red_header using JQuery API
+
+$('div').click(function () {
+  $('header').css('color', '#FF0000');
+});
