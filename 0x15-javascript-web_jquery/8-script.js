65c240e77cfc03b1a21a868a47d94470dc4a6815
Barbara <barbeaboo@gmail.com>
2023-01-26 06:42:31 +0000

    advanced

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,7 +1,5 @@
 // script that fetches and lists the title for all movies by using given URL
 
-$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function(data){
-for ( title = 0, title < len(data), title++ ) {
-$('UL#list_movies').append(`<li>${data.title}</li>`);
-}
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+  $('UL#list_movies').append(data.results.map(movie => `<li>${data.title}</li>`));
 });
---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,7 @@
+//  script that fetches from given URL and displays the value of hello
+
+$('document').ready(function () {
+  $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
+    $('DIV#hello').text(data.hello);
+  });
+});
